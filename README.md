c436af47cecd245fde7d51bf9b9dc5ed72f4ac4f
Vasili Kavaleu <v.kavaleu@naveksoft.com>
2021-11-23 13:04:19 +0300

    Rename main file / remove comment / add readme

---
+++ README.md
@@ -0,0 +1,20 @@
+## TUI configuration wizard
+
+Clone project, and install dependency
+
+```bash
+# Install dependency
+go mod download
+```
+
+Run wizard
+```bash
+go run wizarg.go
+```
+
+Or build and then run wizard
+```bash
+go build wizarg.go
+
+./wizard
+```
---
+++ wizard.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"configurator/gui"
+	"github.com/rivo/tview"
+	"github.com/gdamore/tcell/v2"
+    "log"
+	"os"
+)
+
+
+func run() int {
+
+	tview.Styles.ContrastBackgroundColor = tcell.ColorSlateGrey
+	tview.Styles.MoreContrastBackgroundColor = tcell.ColorTan
+	tview.Styles.BorderColor = tcell.ColorTan
+	tview.Styles.TitleColor = tcell.ColorLightGoldenrodYellow
+
+	gui := gui.New()
+
+	if err := gui.Start(); err != nil {
+		log.Fatal("Cannot start Wizard: %s", err)
+		return 1
+	}
+
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
